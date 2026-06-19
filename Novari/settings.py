83aa306c1baea0ce13feac09eb06bad5a474e9c4
palmhillstry <mohamedgamesar4321@gmail.com>
2026-06-20 02:28:17 +0300

    debugging , cors problem

--- Novari/settings.py
+++ Novari/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'novari_base.apps.NovariBaseConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
+]
+CORS_ALLOWED_ORIGINS = [
+    "https://novari-front.vercel.app/",
 ]
 
 REST_FRAMEWORK = {
@@ -47,6 +51,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
