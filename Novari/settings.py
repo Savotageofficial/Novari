4da66312df9efe44b194245bf456b8534910ccf2
palmhillstry <mohamedgamesar4321@gmail.com>
2026-06-22 23:46:21 +0300

    finishing everything

--- Novari/settings.py
+++ Novari/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jjbois(3$+)9czv8r7r0(8+egvys#k0te6i8n))d=3e-5luv0w'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'novari_db',
         'USER': 'novari_user',
-        'PASSWORD': '1234', #secret
+        'PASSWORD': config('db_pass'), #secret
         'HOST': 'localhost',
         'PORT': '5432',  # default PostgreSQL port
     }
